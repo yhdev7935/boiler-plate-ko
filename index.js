7cf89e9768cafecd6573e2c7ac2b0ceaa7dc7d3b
yhdev7935 <qodydgk8624@kookmin.ac.kr>
2020-10-24 19:03:08 +0900

    add auth/logout

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
+const { auth } = require('./middleware/auth')
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -23,7 +24,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World! @@'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다.
@@ -38,7 +39,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
     // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
@@ -51,7 +52,6 @@ app.post('/login', (req, res) => {
 
         // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인해야함.
         user.comparePassword(req.body.password, (err, isMatch) => {
-            console.log("1", isMatch);
             if(!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
             // 비밀번호까지 맞다면 토큰을 생성하기
             user.generateToken((err, user) => {
@@ -63,10 +63,36 @@ app.post('/login', (req, res) => {
                 .json({ loginSuccess: true, userId: user._id })
             })
         })
-
     })
 })
 
 
+app.get('/api/users/auth', auth, (req, res) => {
+    
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    
+    User.findOneAndUpdate({ _id: req.user._id }, 
+        { token: "" },
+        (err, user) => {
+            if(err) return res.json({ success: false, err});
+            return res.status(200).send({
+                success: true
+            })
+        })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
